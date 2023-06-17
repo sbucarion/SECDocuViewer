3996269d609eaf5b72b822f3b89cd50f36e2174b
Sebastian <sebastian@bucarion.org>
2023-06-17 17:30:31 -0400

    switched from browserrouter to hashrouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from './pages/Home'
 import './index.css'
 
@@ -7,12 +7,13 @@ const App = () => {
     //This template to create as many webpages as you want
     //Each Route is a specific webpage and they call 
     //The respective file that has all webpage info
-    <BrowserRouter basename="/SECDocuViewer">
-      <Routes>
-        {/* Home is for landing page of unauth users */}
+    <div className="App">
+      <HashRouter basename="/SECDocuViewer">
+        <Routes>
+         {/* Home is for landing page of unauth users */}
         <Route path="/" element={<Home/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter></div>
   );
 }
 
