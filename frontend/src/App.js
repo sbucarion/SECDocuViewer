846bfaba8a8d9232618cc88c318a5daef4d71aa4
Sebastian <sebastian@bucarion.org>
2023-06-17 17:40:44 -0400

    yyy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
 import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from './pages/Home'
-import './index.css'
 
 function App() {
   return (
-    //This template to create as many webpages as you want
-    //Each Route is a specific webpage and they call 
-    //The respective file that has all webpage info
     <div className="App">
-      <h1>hello</h1>
+      <h1> Hello </h1>
       <HashRouter>
         <Routes>
-         {/* Home is for landing page of unauth users */}
-        <Route path="/" element={<Home/>}/>
-      </Routes>
-    </HashRouter></div>
+          {/* Home is for landing page of unauth users */}
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </HashRouter>
+    </div>
   );
 }
+
 
 export default App;
