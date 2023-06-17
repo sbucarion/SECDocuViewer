eaa4f1db4049bd231cf8e279716a6cd2ce8f6270
Sebastian <sebastian@bucarion.org>
2023-06-17 17:35:05 -0400

    y

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from './pages/Home'
 import './index.css'
 
-const App = () => {
+function App() {
   return (
     //This template to create as many webpages as you want
     //Each Route is a specific webpage and they call 
     //The respective file that has all webpage info
     <div className="App">
+      <h1>hello</h1>
       <HashRouter basename="/SECDocuViewer">
         <Routes>
          {/* Home is for landing page of unauth users */}
