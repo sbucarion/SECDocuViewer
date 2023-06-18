1f80fd1e3f89f25979a029ff5b4b2093834a5f80
Sebastian <sebastian@bucarion.org>
2023-06-17 20:40:48 -0400

    testing vercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,24 @@ import './App.css';
 import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from './pages/Home'
 
+
 function App() {
-  return (
-    <div className="App">
-      <h1> Hello </h1>
-      <HashRouter>
-        <Routes>
-          {/* Home is for landing page of unauth users */}
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </HashRouter>
-    </div>
-  );
+  return (<h1>Hello</h1>)
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1> Hello </h1>
+//       <HashRouter>
+//         <Routes>
+//           {/* Home is for landing page of unauth users */}
+//           <Route path="/" element={<Home />} />
+//         </Routes>
+//       </HashRouter>
+//     </div>
+//   );
+// }
 
   // return (
   //   <div className="App">
