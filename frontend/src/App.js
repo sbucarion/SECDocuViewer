65002d1cba394ccb64494cc60de8e7856b7f1012
Sebastian <sebastian@bucarion.org>
2023-06-17 20:52:04 -0400

    should be good to go fro vercel i hope

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,35 +3,19 @@ import './App.css';
 import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from './pages/Home'
 
-
 function App() {
-  return (<h1>Hello</h1>)
+  return (
+    <div className="App">
+      <h1> Hello </h1>
+      <HashRouter>
+        <Routes>
+          {/* Home is for landing page of unauth users */}
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </HashRouter>
+    </div>
+  );
 }
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1> Hello </h1>
-//       <HashRouter>
-//         <Routes>
-//           {/* Home is for landing page of unauth users */}
-//           <Route path="/" element={<Home />} />
-//         </Routes>
-//       </HashRouter>
-//     </div>
-//   );
-// }
-
-  // return (
-  //   <div className="App">
-  //     <h1> Hello </h1>
-  //     <BrowserRouter>
-  //      <Routes>
-  //       {/* Home is for landing page of unauth users */}
-  //        <Route path="/" element={<Home/>}/>
-  //      </Routes>
-  //     </BrowserRouter>
-  //   </div>
-  // );
 
 export default App;
